514b69b322abea10355bc21a6b8bebbbb6ca65b4
Mohanraj209 <mohanraj1715@gmail.com>
2024-01-24 16:50:19 +0530

    [MOSIP-30431] Updated Automation scripts for cluster deployment.
    Signed-off-by: Mohanraj209 <mohanraj1715@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,4 @@
 
 # Run RKE
 rke up --config /rke/config/cluster.yml
-ls
-pwd
-# Copy generated files to the shared volume
-cp cluster.rkestate /rke/config/
-cp kube_config_cluster.yml /rke/config/
+
