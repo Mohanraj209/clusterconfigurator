8fd0b4649e61db7aa13835269558756f1855d49a
Mohanraj209 <mohanraj1715@gmail.com>
2024-01-25 11:35:17 +0530

    [MOSIP-30431] Updated Automation scripts for cluster deployment.
    Signed-off-by: Mohanraj209 <mohanraj1715@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@
 rke up --config /rke/config/cluster.yml
 cd /rke
 ls -l
-chmod -R 777 /rke/config/
+chmod -R 777 /rke/config/cluster.rkestate /rke/config/kube_config_cluster.yml
 rm -rf ssh_key runner_key.pub runner_key
